f4f477d4eb47d96375dfe3f66a6d9b209cce8ac7
chiq2045 <charles.oroko@outlook.com>
2021-10-23 11:02:59 +0300

    add babel-typescript

--- webpack.config.ts
+++ webpack.config.ts
@@ -50,7 +50,11 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env', '@babel/preset-react']
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+                '@babel/preset-typescript'
+              ]
             }
           }
         ]
